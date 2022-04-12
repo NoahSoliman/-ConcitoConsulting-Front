eaab94c5d4d4b62bfd9881a17d4327579d52972d
Mohammad-sloiman <mohammad.soliman@utb.ecutbildning.se>
2022-04-13 01:23:25 +0200

    fix savr prio and note and process i state

--- src/components/Companies.js
+++ src/components/Companies.js
@@ -1,11 +1,12 @@
 import React from "react";
+import "./Companies.css";
 
 import { useParams, useLocation } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import axios from "axios";
 import { Routes, Route, Link, useNavigate } from "react-router-dom";
-import { Form, Pagination, PageItem, Table } from "react-bootstrap";
+import { Form, Pagination, PageItem, Table, InputGroup } from "react-bootstrap";
 
 import {
   selectedPage,
@@ -24,35 +25,46 @@ function Companies(props) {
   const [maxNum, setMaxNum] = useState(10);
   const [totalPage, setTotalPage] = useState("");
   const [allCompanies, setAllCompanies] = useState([]);
+  const [process, setProcess] = useState(false);
+  const [prioChecket, setPrioChecket] = useState(false);
   const [allTaHead, setAllTaHead] = useState([
-
-    "OrganizationOrgnr",
+    // "OrganizationOrgnr",
     "OrganizationName",
-    "OrganizationStreetName",
-    "OrganizationZIPCode",
-    "OrganisationKommun",
-    "OrganizationCity",
-    "OrganizationRegion",
-    "OrganizationTelefon",
+    "Bearbeta ej",
+    "PRIO",
+    "Ev. Not till Concito",
+    // "OrganizationStreetName",
+    // "OrganizationZIPCode",
+    // "OrganisationKommun",
+    // "OrganizationCity",
+    // "OrganizationRegion",
+    // "OrganizationTelefon",
     "OrganizationWeb",
-    "OrganizationMail",
+    // "OrganizationMail",
     "OrganizationBransch",
-    "OrganizationSNI1",
+    // "OrganizationSNI1",
     "OrganizationEmployees",
     "OrganizationTurnover",
-    "RegNumberOfDigits",
-    "SNI2Numbers",
-    "SNI3Numbers",
-    "SNI4Numbers",
-    "SNI5Numbers",
-    "OrganizationV1W1",
-    "Postadress",
-    "Postnummer",
-    "Stad",
-    "FinancialYear",
-    "Resultat",
-
+    "Moderbolag",
+    // "RegNumberOfDigits",
+    // "SNI2Numbers",
+    // "SNI3Numbers",
+    // "SNI4Numbers",
+    // "SNI5Numbers",
+    // "OrganizationV1W1",
+    // "Postadress",
+    // "Postnummer",
+    // "Stad",
+    // "FinancialYear",
+    // "Resultat",
+    // "SNIKod2",
+    // "SNIKod2Description",
+    // "Etablerades",
+    // "EBITDA",
+    // "Soliditet",
   ]);
+
+  let [extraOptions, setExtraOptions] = useState([]);
 
   let navigate = useNavigate();
 
@@ -64,8 +76,8 @@ function Companies(props) {
 
   useEffect(() => {
     let getData = async () => {
-      console.log("useEfffect");
-      console.log(`http://localhost:3000/api/company/${reducerState.page}/`);
+      // console.log("useEfffect");
+      // console.log(`http://localhost:3000/api/company/${reducerState.page}/`);
       try {
         const res = await axios.post(
           `http://localhost:3000/api/company/${reducerState.page}/`,
@@ -77,8 +89,8 @@ function Companies(props) {
 
         if (res.statusText === "OK") {
           console.log("res.statusText === OK");
-          console.log(res.data);
-          console.log(res.data.total);
+          // console.log(res.data);
+          // console.log(res.data.total);
           setTotalPage(Math.ceil(res.data.total / 50));
           setAllCompanies(res.data.companies);
           dispatch(selectedCompanies(res.data));
@@ -87,7 +99,7 @@ function Companies(props) {
         }
       } catch (err) {
         console.log(err);
-        console.log("error post");
+        // console.log("error post");
         navigate("/company");
       }
     };
@@ -137,8 +149,8 @@ function Companies(props) {
   }
 
   function prevPage() {
-    console.log(maxNum);
-    console.log(num);
+    // console.log(maxNum);
+    // console.log(num);
     if (maxNum > 10) {
       setMaxNum(maxNum - 10);
       setNum(num - 10);
@@ -147,8 +159,8 @@ function Companies(props) {
     }
   }
   function nextPage() {
-    console.log("maxNum" + maxNum);
-    console.log(num);
+    // console.log("maxNum" + maxNum);
+    // console.log(num);
     if (maxNum >= 10 && maxNum <= totalPage) {
       setMaxNum(maxNum + 10);
       setNum(num + 10);
@@ -157,48 +169,368 @@ function Companies(props) {
     }
   }
 
+  let clickHandleProcess = (e, OrganizationOrgnr, index) => {
+    let prioElement = document.getElementById(`${OrganizationOrgnr}prio`);
+    let processElement = document.getElementById(`${OrganizationOrgnr}process`);
+    let notProcesState = processElement.checked;
+
+    if (notProcesState) prioElement.checked = false;
+
+    const found = extraOptions.find(
+      (element) => element.OrganizationOrgnr === OrganizationOrgnr
+    );
+    if (found) {
+      found.notProcesseState = processElement.checked;
+      found.prioState = prioElement.checked;
+
+      if (notProcesState) {
+        setExtraOptions((prev) => [
+          ...prev.filter(
+            (item) => item.OrganizationOrgnr !== OrganizationOrgnr
+          ),
+          found,
+        ]);
+      } else if (!notProcesState && found.note) {
+        setExtraOptions((prev) => [
+          ...prev.filter(
+            (item) => item.OrganizationOrgnr !== OrganizationOrgnr
+          ),
+          {
+            OrganizationOrgnr: OrganizationOrgnr,
+            note: found.note,
+          },
+        ]);
+      } else if (!notProcesState && !found.note) {
+        setExtraOptions((prev) => [
+          ...prev.filter(
+            (item) => item.OrganizationOrgnr !== OrganizationOrgnr
+          ),
+        ]);
+      }
+    } else {
+      setExtraOptions((prev) => [
+        ...prev,
+        {
+          OrganizationOrgnr: OrganizationOrgnr,
+          notProcesseState: processElement.checked,
+          prioState: prioElement.checked,
+        },
+      ]);
+    }
+  };
+
+  let clickHandPrio = (e, OrganizationOrgnr, index) => {
+    let processElement = document.getElementById(`${OrganizationOrgnr}process`);
+    let prioElement = document.getElementById(`${OrganizationOrgnr}prio`);
+    let prioStete = prioElement.checked;
+
+    if (prioStete) processElement.checked = false;
+
+    const found = extraOptions.find(
+      (element) => element.OrganizationOrgnr === OrganizationOrgnr
+    );
+
+    if (found) {
+      found.notProcesseState = processElement.checked;
+      found.prioState = prioElement.checked;
+
+      if (prioStete) {
+        setExtraOptions((prev) => [
+          ...prev.filter(
+            (item) => item.OrganizationOrgnr !== OrganizationOrgnr
+          ),
+          found,
+        ]);
+      } else if (!prioStete && found.note) {
+        setExtraOptions((prev) => [
+          ...prev.filter(
+            (item) => item.OrganizationOrgnr !== OrganizationOrgnr
+          ),
+          {
+            OrganizationOrgnr: OrganizationOrgnr,
+            note: found.note,
+          },
+        ]);
+      } else if (!prioStete && !found.note) {
+        setExtraOptions((prev) => [
+          ...prev.filter(
+            (item) => item.OrganizationOrgnr !== OrganizationOrgnr
+          ),
+        ]);
+      }
+    } else {
+      setExtraOptions((prev) => [
+        ...prev,
+        {
+          OrganizationOrgnr: OrganizationOrgnr,
+          notProcesseState: processElement.checked,
+          prioState: prioElement.checked,
+        },
+      ]);
+    }
+  };
+
+  let clickHandleNote = (e, OrganizationOrgnr, index) => {
+    let note = e.target.value;
+
+    console.log(OrganizationOrgnr);
+
+    const found = extraOptions.find(
+      (element) => element.OrganizationOrgnr === OrganizationOrgnr
+    );
+
+    // if (note) {
+
+    if (found && note) {
+      found.note = note;
+
+      console.log(found);
+
+      setExtraOptions((prev) => [
+        ...prev.filter((item) => item.OrganizationOrgnr !== OrganizationOrgnr),
+
+        found,
+      ]);
+    } else if (!found && note) {
+      setExtraOptions((prev) => [
+        ...prev,
+        {
+          OrganizationOrgnr: OrganizationOrgnr,
+
+          note: note,
+        },
+      ]);
+    } else if ((found.prioState || found.notProcesseState) && !note) {
+      console.log("((found.prioState || found.notProcesState) && !note)");
+      setExtraOptions((prev) => [
+        ...prev.filter((item) => item.OrganizationOrgnr !== OrganizationOrgnr),
+        {
+          OrganizationOrgnr: OrganizationOrgnr,
+          notProcesseState: found.notProcesseState,
+          prioState: found.prioState,
+        },
+      ]);
+    } else if (found && !note) {
+      setExtraOptions((prev) => [
+        ...prev.filter((item) => item.OrganizationOrgnr !== OrganizationOrgnr),
+      ]);
+    }
+
+    // setExtraOptions((prev) => [
+    //   ...prev.filter(
+    //     (item) => item.OrganizationOrgnr !== OrganizationOrgnr
+    //   ),
+    //   {
+    //     OrganizationOrgnr: OrganizationOrgnr,
+
+    //     note: found.note,
+    //   },
+    // ]);
+    // }
+
+    // البحث عن رقم الشركة
+    // في حال كانت مصفوفة
+    // علي ان اعدل علي خيار النوت يلي فيها
+    // ومن ثم حذفها القديمة من الستيت
+    // واضافة الجديدة
+  };
+  let sendData = () => {
+    console.log(reducerState.customerBranchChoice);
+
+    /* 
+jag ska bygga en sån objec
+{ OrgNum:34242,
+prio: true,
+dontProccess: false,
+note:""}
+
+dessa OBJ ska skickas till backend med alla SNI3 som jag har redan
+
+i backend jag ska hämta data för SNI3 och filtera med alla orgNumber för att plocka ut det företag vi vill
+
+att lägga prio eller ej arbete eller note till dem
+
+sedan jag kan använda find för att hitta varje företag och lägga till dessa data (prio osv.. ) till det. 
+
+
+
+
+
+
+
+*/
+  };
+
   return (
     <div>
       <h1>Companies</h1>
-
+      <button
+        type="button"
+        onClick={() => {
+          localStorage.removeItem("Logins State");
+          navigate("/login");
+        }}
+      >
+        Logout
+      </button>
+      <br />
+      <br />
+      <button type="button" onClick={sendData}>
+        Skicka
+      </button>
+      <div className="pagination-container">
+        <div>
+          <Pagination>
+            {/* <Pagination.First /> */}
+            <Pagination.Prev onClick={prevPage} />
+            {/* <Pagination.Ellipsis /> */}
+            {itemsState}
+            {/* <Pagination.Ellipsis /> */}
+            <Pagination.Next onClick={nextPage} />
+            {/* <Pagination.Last /> */}
+          </Pagination>
+        </div>
+      </div>
       <div>
-        <Table responsive>
+        <Table responsive striped bordered hover size="sm">
           <thead>
             <tr>
               <th>#</th>
               {allTaHead.map((title, index) => (
-                  
                 <th key={index}>{title}</th>
-              ))}  
+              ))}
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>1</td>
-              {Array.from({ length: 12 }).map((_, index) => (
-                <td key={index}>Table cell {index}</td>
-              ))}
-            </tr>
-            <tr>
-              <td>2</td>
-              {Array.from({ length: 12 }).map((_, index) => (
-                <td key={index}>Table cell {index}</td>
-              ))}
-            </tr>
+            {allCompanies
+              ? allCompanies.map(
+                  (
+                    {
+                      OrganizationOrgnr,
+                      OrganizationName,
+
+                      //   OrganizationStreetName,
+                      //   OrganizationZIPCode,
+                      //   OrganisationKommun,
+                      //   OrganizationCity,
+                      //   OrganizationRegion,
+                      //   OrganizationTelefon,
+                      OrganizationWeb,
+                      //   OrganizationMail,
+                      OrganizationBransch,
+                      //   OrganizationSNI1,
+                      OrganizationEmployees,
+                      OrganizationTurnover,
+                      Moderbolag,
+
+                      //   RegNumberOfDigits,
+                      //   SNI2Numbers,
+                      //   SNI3Numbers,
+                      //   SNI4Numbers,
+                      //   SNI5Numbers,
+                      //   OrganizationV1W1,
+                      //   Postadress,
+                      //   Postnummer,
+                      //   Stad,
+                      //   FinancialYear,
+                      //   Resultat,
+                      //   SNIKod2,
+                      //   SNIKod2Description,
+                      //   Etablerades,
+                      //   EBITDA,
+                      //   Soliditet,
+                    },
+                    index
+                  ) => (
+                    <tr key={index}>
+                      <td>{index + 1}</td>
+                      {/* <td> {OrganizationOrgnr}</td> */}
+                      <td> {OrganizationName}</td>
+
+                      <td>
+                        <Form.Check
+                          type={"checkbox"}
+                          id={OrganizationOrgnr + "process"}
+                          //   label="Select All"
+                          onClick={(e) =>
+                            clickHandleProcess(e, OrganizationOrgnr, index)
+                          }
+                        />
+                      </td>
+                      <td>
+                        <Form.Check
+                          type={"checkbox"}
+                          id={OrganizationOrgnr + "prio"}
+                          onClick={(e) =>
+                            clickHandPrio(e, OrganizationOrgnr, index)
+                          }
+
+                          //   checked={prioChecket}
+                          //   id={"selectAll"}
+                          //   label="Select All"
+                          //   disabled={process}
+                          //   onChange={()=>{this.checked(!prioChecket)}}
+                        />
+                      </td>
+                      <td>
+                        <Form.Control
+                          id={index}
+                          onChange={(e) =>
+                            clickHandleNote(e, OrganizationOrgnr, index)
+                          }
+
+                          //   disabled={process}
+                        />
+                      </td>
+
+                      {/* <td> {OrganizationStreetName}</td> */}
+                      {/* <td> {OrganizationZIPCode}</td> */}
+                      {/* <td> {OrganisationKommun}</td> */}
+                      {/* <td> {OrganizationCity}</td> */}
+                      {/* <td> {OrganizationRegion}</td> */}
+                      {/* <td> {OrganizationTelefon}</td> */}
+                      <td> {OrganizationWeb}</td>
+                      {/* <td> {OrganizationMail}</td> */}
+                      <td> {OrganizationBransch}</td>
+                      {/* <td> {OrganizationSNI1}</td> */}
+                      <td> {OrganizationEmployees}</td>
+                      <td> {OrganizationTurnover}</td>
+                      <td> {Moderbolag}</td>
+                      {/* <td> {RegNumberOfDigits}</td> */}
+                      {/* <td> {SNI2Numbers}</td> */}
+                      {/* <td> {SNI3Numbers}</td> */}
+                      {/* <td> {SNI4Numbers}</td> */}
+                      {/* <td> {SNI5Numbers}</td> */}
+                      {/* <td> {OrganizationV1W1}</td> */}
+                      {/* <td> {Postadress}</td> */}
+                      {/* <td> {Postnummer}</td> */}
+                      {/* <td> {Stad}</td> */}
+                      {/* <td> {FinancialYear}</td> */}
+                      {/* <td> {Resultat}</td> */}
+                      {/* <td> {SNIKod2}</td> */}
+                      {/* <td> {SNIKod2Description}</td> */}
+                      {/* <td> {Etablerades}</td> */}
+                      {/* <td> {EBITDA}</td> */}
+                      {/* <td> {Soliditet}</td> */}
+                    </tr>
+                  )
+                )
+              : ""}
           </tbody>
         </Table>
       </div>
-
-      <div>
-        <Pagination>
-          {/* <Pagination.First /> */}
-          <Pagination.Prev onClick={prevPage} />
-          {/* <Pagination.Ellipsis /> */}
-          {itemsState}
-          {/* <Pagination.Ellipsis /> */}
-          <Pagination.Next onClick={nextPage} />
-          {/* <Pagination.Last /> */}
-        </Pagination>
+      <div className="pagination-container">
+        <div>
+          <Pagination>
+            {/* <Pagination.First /> */}
+            <Pagination.Prev onClick={prevPage} />
+            {/* <Pagination.Ellipsis /> */}
+            {itemsState}
+            {/* <Pagination.Ellipsis /> */}
+            <Pagination.Next onClick={nextPage} />
+            {/* <Pagination.Last /> */}
+          </Pagination>
+        </div>
       </div>
     </div>
   );
